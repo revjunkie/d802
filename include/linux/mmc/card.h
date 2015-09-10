6d39cf8cc86acd4fd7c8ee1d894bb23fc05cd3e1
Devin Kim <dojip.kim@lge.com>
2015-09-09 21:11:02 -0700

    mmc: workaround for read ahead issue
    When the sequential read is interrupted by a special operation (RPMB access,
    Secure Erase and Secure Trim) the read ahead feature does not function as
    intended. In that case, data from special operation is not removed from the
    internal buffer. So when we read the next address, the reading data from the
    previous operation instead of the address.
    
    To avoid the issue, add dummy read and ignore the data in that case.
    
    Change-Id: I6c1a3285034c9e057879de601e1f80a4bed07edb
    Signed-off-by: Devin Kim <dojip.kim@lge.com>

--- include/linux/mmc/card.h
+++ include/linux/mmc/card.h
@@ -350,7 +350,8 @@ struct mmc_card {
 #define MMC_QUIRK_LONG_READ_TIME (1<<9)		/* Data read time > CSD says */
 #define MMC_QUIRK_SEC_ERASE_TRIM_BROKEN (1<<10)	/* Skip secure for erase/trim */
 						/* byte mode */
-#define MMC_QUIRK_INAND_DATA_TIMEOUT  (1<<8)    /* For incorrect data timeout */
+#define MMC_QUIRK_INAND_DATA_TIMEOUT  (1<<11)	/* For incorrect data timeout */
+#define MMC_QUIRK_BLK_NEED_DUMMY_READ  (1<<12)	/* Issue dummy read in some cases */
 /* To avoid eMMC device getting broken permanently due to HPI feature */
 #define MMC_QUIRK_BROKEN_HPI (1 << 11)
  /* Skip data-timeout advertised by card */
